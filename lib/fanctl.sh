32f5e7b731ef0c59c972935795935ef8532f1c8a
Mr H. <danymortul2011@hotmail.com>
2026-01-10 22:54:28 +0100

    Update fanctl.sh
    Check dependencies

--- lib/fanctl.sh
+++ lib/fanctl.sh
@@ -3,11 +3,41 @@
 #############################################
 # Fan Control Module (HomePinas)
 #############################################
+check_fanctl_dependencies() {
+
+    info_msg "Checking fan control dependencies..."
+
+    local missing_deps=()
+
+    if ! command -v smartctl &>/dev/null; then
+        missing_deps+=("smartmontools")
+    fi
+
+    if [ ${#missing_deps[@]} -gt 0 ]; then
+        warning_msg "Installing missing dependencies: ${missing_deps[*]}"
+        apt-get update -qq
+        if apt-get install -y "${missing_deps[@]}"; then
+            success_msg "Fan control dependencies installed."
+        else
+            error_msg "Failed to install fan control dependencies."
+            return 1
+        fi
+    else
+        success_msg "All fan control dependencies are present."
+    fi
+
+    return 0
+}
 
 install_fanctl() {
 
     info_msg "Installing HomePinas Fan Control..."
-
+    
+    if ! check_fanctl_dependencies; then
+        error_msg "Cannot continue without required dependencies."
+        return 1
+    fi
+    
     local SERVICE_NAME="homepinas-fanctl"
     local INSTALL_PATH="/usr/local/bin/homepinas-fanctl.sh"
 
